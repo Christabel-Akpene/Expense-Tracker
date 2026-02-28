40a579f52b61ee808e6536522caaf80dc864105a
Christabel-Akpene <dc.akpene@gmail.com>
2026-02-28 17:55:37 +0000

    style: add montserrat font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
 import "./global.css"
 import { Stack } from "expo-router";
+import {useFonts, Montserrat_400Regular, Montserrat_700Bold} from "@expo-google-fonts/montserrat"
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen"
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Montserrat_400Regular, Montserrat_700Bold
+  })
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null
+  }
+  
   return <Stack />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading_font: ["Montserrat_700Bold"],
+        text_font: ["Montserrat_400Regular"],
+      },
+    },
   },
   plugins: [],
-}
+};
 
